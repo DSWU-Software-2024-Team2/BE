9aee417fa79ef433aee797247f70fe4ba33d2f72
안희원 <sallyinner59@duksung.ac.kr>
2024-10-11 13:23:56 +0900

    '.'

--- App.js
+++ App.js
@@ -2,22 +2,36 @@
 const express = require('express');
 const cors = require('cors'); // CORS 모듈 추가
 const authRoutes = require('./routers/authRouter');
+
+const infoRoutes = require('./routers/infoRouter');
+const communityRoutes = require('./routers/communityRouter');
 const tipRoutes = require('./routers/tipRouter');
-const searchRoutes = require('./routers/searchRouter');
+
 const postRoutes = require('./routers/postRouter');
 const reactionRoutes = require('./routers/reactionRouter');
+const searchRoutes = require('./routers/searchRouter');
+
+const cartRoutes = require('./routers/cartRouter');
+const payRoutes = require('./routers/payRouter');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors()); 
 app.use(express.json()); 
 
 app.use('/api/auth', authRoutes);
+
+app.use('/api/info', infoRoutes);
 app.use('/api/tips', tipRoutes);
+app.use('/api/communities', communityRoutes);
+
 app.use('/api/search', searchRoutes);
 app.use('/api/post', postRoutes);
 app.use('/api/react', reactionRoutes);
+
+app.use('/api/cart', cartRoutes);
+app.use('/api/payment', payRoutes);
 
 // 전역 에러 핸들러
 app.use((err, req, res, next) => {
